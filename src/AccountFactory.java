e566d4f3f3ce749d2455e046611bc635f91d91d2
Clebson Moura <clebson.sh@gmail.com>
2025-03-04 15:51:30 -0300

    add a second account and better log

--- src/AccountFactory.java
+++ src/AccountFactory.java
@@ -1,9 +1,13 @@
 public class AccountFactory {
     public static void main(String[] args) {
         Account firstAccount = new Account();
-
         firstAccount.balance = 120.50;
+        firstAccount.balance += 79.50;
 
-        System.out.println(firstAccount.balance);
+        Account secondAccount = new Account();
+        secondAccount.balance = 50.00;
+
+        System.out.println("First account has: $" + firstAccount.balance);
+        System.out.println("Secound account has: $" + secondAccount.balance);
     }
 }

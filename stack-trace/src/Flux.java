a5134aeeab6a8dcbccedea5b38e7844c68e05dcf
Clebson Moura <clebson.sh@gmail.com>
2025-03-11 21:41:47 -0300

    Creating a custom exception

--- stack-trace/src/Flux.java
+++ stack-trace/src/Flux.java
@@ -3,7 +3,7 @@ public class Flux {
         System.out.println("Main started");
         try {
             method1();
-        } catch (ArithmeticException | NullPointerException e) {
+        } catch (ArithmeticException | NullPointerException | MyException e) {
             String msg = e.getMessage();
             System.out.println("Exception message: " + msg);
             e.printStackTrace();
@@ -11,20 +11,17 @@ public class Flux {
         System.out.println("end of main");
     }
 
-    private static void method1() {
+    private static void method1() throws MyException {
         System.out.println("Method1 started");
         method2();
         System.out.println("end of method1");
     }
 
-    private static void method2() {
+    private static void method2() throws MyException {
         System.out.println("Method2 started");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
 
-            Account account = null;
-            account.deposit(i);
-        }
-        System.out.println("end of method2");
+        throw new MyException("Boom!");
+
+        // System.out.println("end of method2");
     }
 }
